7b9500dbaaa1bc42b68d786f6d73a51596d51571
boygl34 <56708291+boygl34@users.noreply.github.com>
2023-12-15 09:15:28 +0700

    ád

--- src/lib/webSocketConnection.js
+++ src/lib/webSocketConnection.js
@@ -1,6 +1,6 @@
 import ioClient from 'socket.io-client';
-const ENDPOINT = 'http://localhost:3000/';
-
+//const ENDPOINT = 'http://localhost:3000/';
+const ENDPOINT = 'https://chatapp-ek0.pages.dev/';
 const socket = ioClient(ENDPOINT);
 
 export const io = socket;
